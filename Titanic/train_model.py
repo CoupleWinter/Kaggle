751c29bb68b2d0d0ae73760e0a29326c264d0b7f
Aisuko Lee <urakiny@gmail.com>
2018-01-23 23:08:02 +0800

    Modify training functions :desert:

--- Titanic/train_model.py
+++ Titanic/train_model.py
@@ -9,21 +9,20 @@ from tqdm import tqdm
 session = tf.InteractiveSession()
 
 x = tf.placeholder(dtype=tf.float32, shape=[None, 6], name='feature')
-y = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='y')
+y_ = tf.placeholder(dtype=tf.float32, shape=[None, 2], name='y')
 
 w = tf.Variable(initial_value=tf.random_normal([6, 2]), name='weight')
 b = tf.Variable(initial_value=tf.random_normal([2]), name='bias')
 
-cross = tf.nn.softmax_cross_entropy_with_logits(tf.matmul(x * w) + b)
-
-train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cross)
+cross = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=tf.matmul(x * w) + b, )
+cross_entropy = tf.reduce_mean(cross)
+train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
 
 tf.global_variables_initializer().run()
 
 for x in tqdm(10):
-    feed={x:''}
-    session.run([train_op],feed=feed)
-
+    feed = {x: '', y_: ''}
+    _, loss = session.run([train_op, cross_entropy], feed=feed)
 
 if __name__ == '__main__':
     print(x)
