4837bacf169685a675cc8f850ca5b7d516218de4
Noctis Lucis <urakiny@gmail.com>
2018-02-12 14:27:53 +0800

    Add import data struct

--- Titanic/train_model.py
+++ Titanic/train_model.py
@@ -4,7 +4,21 @@
 
 
 import tensorflow as tf
+import pandas as pd
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('data/train.csv')
+
+data = data.fillna(0)
+data['Sex'] = data['Sex'].apply(lambda s: 1 if s == 'male' else 0)
+data['Deceased'] = data['Survived'].apply(lambda s: 1 - s)
+dataset_X = data[['Sex', 'Age', 'Pclass', 'SibSp', 'Parch', 'Fare']].as_matrix()
+dataset_Y = data[['Deceased', 'Survived']].as_matrix()
+
+# split training data and validation set data
+
+X_train, X_val, y_train, y_val = train_test_split(dataset_X, dataset_Y, test_size=0.2, random_state=42)
 
 session = tf.InteractiveSession()
 
@@ -19,13 +33,18 @@ y_pred = tf.nn.softmax(tf.matmul(X, w) + b)
 cross_entropy = -tf.reduce_sum(Y * tf.log(y_pred + 1e-10), reduction_indices=1)
 cost = tf.reduce_mean(cross_entropy)
 
-train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+
+# calculate accuracy
+correct_pred = tf.equal(tf.argmax(Y, 1), tf.argmax(y_pred, 1))
+acc_op = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 tf.global_variables_initializer().run()
 
-for x in tqdm(10):
-    feed = {x: '', y_pred: ''}
-    _, loss = session.run([train_op, cross_entropy], feed=feed)
-
-if __name__ == '__main__':
-    print(x)
+for i in range(10):
+    total_loss = 0.
+    for j in range(10):
+        feed_dict = {X: [X_train[i]], Y: [y_train[i]]}
+        _, loss = session.run([train_op, cost], feed_dict=feed_dict)
+        total_loss += loss
+    print('')
